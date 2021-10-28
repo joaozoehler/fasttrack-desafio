70ca2a04b0d4fcc6ef51f53a5c5b3d25401ed0bd
joaozoehler <89044655+joaozoehler@users.noreply.github.com>
2021-10-28 10:49:50 -0300

    v. 4 do programa
    - aprimoramentos gerais
    1) substituição de amount = 0 por NULL
    2) substituição de gender = E e U por NULL
    3) substituição de age = U por NULL

--- programa_DownUnzipInsert.py
+++ programa_DownUnzipInsert.py
@@ -4,7 +4,6 @@ import kaggle
 import zipfile
 import pandas as pd
 import mysql.connector as msql
-import mysql.connector
 from mysql.connector import Error
 
 # Download do dataset via Kaggle API
@@ -28,18 +27,18 @@ dataset = pd.read_csv(f'{arquivo}', delimiter = ',', quotechar = "'", index_col 
 
 # Tentativa de conexão no banco e criação do schema
 try:
-    conn = msql.connect(host='localhost', port='3306', user='admin', password='admin')
-    if conn.is_connected():
-        cursor = conn.cursor()
+    conexao = msql.connect(host='localhost', port='3306', user='admin', password='admin')
+    if conexao.is_connected():
+        cursor = conexao.cursor()
         cursor.execute(f"CREATE DATABASE {nome_schema}")
         print("O banco de dados foi criado com sucesso.")
 except Error as e:
     print("Error while connecting to MySQL", e)
 
 # Conexão no banco e schema
-conn = mysql.connector.connect(host='localhost', port='3306', database=nome_schema, user='admin', password='admin')
-if conn.is_connected():
-    cursor = conn.cursor()
+conexao = msql.connect(host='localhost', port='3306', database=nome_schema, user='admin', password='admin')
+if conexao.is_connected():
+    cursor = conexao.cursor()
     cursor.execute("select database();")
     record = cursor.fetchone()
     print(f"Sua conexão ao banco {record} foi estabelecida")
@@ -65,6 +64,45 @@ if conn.is_connected():
         cursor.execute(sql, tuple(row))
         print(f"Dado inserido")
 
-# Adiciona coluna "id" com incrementação automática
+# Adiciona coluna "id" (INT) com incrementação automática
     cursor.execute(f"ALTER TABLE {nome_tabela} ADD id INT NOT NULL PRIMARY KEY AUTO_INCREMENT FIRST")
-conn.commit()
+
+# Adiciona a coluna "ano" com o valor "2021" (SMALLINT) default
+    cursor.execute(f"ALTER TABLE {nome_tabela} ADD ano SMALLINT DEFAULT 2021")
+
+# Adiciona a coluna "mes" sem valor com o type (SMALLINT)
+    cursor.execute(f"ALTER TABLE {nome_tabela} ADD mes SMALLINT")
+
+# Adiciona a coluna "dia" com o valor "1" (SMALLINT) default
+    cursor.execute(f"ALTER TABLE {nome_tabela} ADD dia SMALLINT DEFAULT 1")
+
+# Adiciona os valores na coluna "mes" conforme dados da coluna "step"
+    cursor.execute(f"UPDATE {nome_tabela}\
+	                SET mes = CASE\
+                    WHEN (step < 31) THEN 1\
+                    WHEN (step > 30) AND (step < 61) THEN 2\
+                    WHEN (step > 60) AND (step < 91) THEN 3\
+                    WHEN (step > 90) AND (step < 121) THEN 4\
+                    WHEN (step > 120) AND (step < 151) THEN 5\
+                    WHEN (step > 150) AND (step < 181) THEN 6\
+                    END")
+
+# Adiciona a coluna "data" via concatenação
+    cursor.execute(f"ALTER TABLE {nome_tabela}\
+                    ADD data VARCHAR(255)\
+                        DEFAULT (CONCAT(ano, '-', mes, '-', '1'))")
+
+# Deixar NULL onde coluna "amount" for 0 (zero)
+cursor.execute(f"UPDATE {nome_tabela}\
+                SET amount = NULL WHERE amount = 0")
+
+cursor.execute(f"UPDATE {nome_tabela}\
+                SET age = NULL WHERE age = 'U'")
+
+cursor.execute(f"UPDATE {nome_tabela}\
+                SET gender = NULL WHERE gender = 'U'")
+
+cursor.execute(f"UPDATE {nome_tabela}\
+                SET gender = NULL WHERE gender = 'E'")
+
+conexao.commit()
